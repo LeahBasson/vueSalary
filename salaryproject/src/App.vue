e9fb663cbb58140c30dcced3f7ff7c729f676d86
LeahBasson <leahbasson54@gmail.com>
2024-06-19 16:35:39 +0200

    vue salary

---
+++ salaryproject/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <form class="container">
+    <input type="number" placeholder="Enter rate" required v-model="rate">
+    <input type="number" placeholder="Enter hours" required v-model="hours">
+    <label output></label>
+    <button @click.prevent="calculateSalary">Calculate</button>
+    <button>Clear</button>
+    </form>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data(){
+    return{
+      rate: '',
+      hours: ''
+    }
+  },
+ methods: {
+     calculateSalary(){
+      document.querySelector('[output]').textContent = this.rate * this.hours
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.container{
+  display: grid;
+  gap: .2rem;
+  width: 30rem;
+  margin-inline: auto;
+  & input{
+    text-align: center;
+  }
+}
+</style>
